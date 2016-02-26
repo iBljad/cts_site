10ad84b0ad02db764434926e31a41e133b6b4fe3
iBljad <drakonmail@gmail.com>
2016-02-26 11:16:32 +0300

    Update forms.py

--- cts_app/forms.py
+++ cts_app/forms.py
@@ -34,3 +34,12 @@ class GamesDD(forms.Form):
         self.fields['game'].choices = [(p.pk, p.game) for p in self.games_list]
         self.fields['platform'].choices = [(p.pk) for p in [self.platform_id]]
         self.fields['platform'].initial = self.platform_id.id
+
+class SearchForm(forms.Form):
+	platforms = [(p.pk, p.platform) for p in Platform.objects.all()] + [('','---------')]
+	games = [(p.pk, p.game) for p in Game.objects.all()] + [('','---------')]
+	platform = forms.ChoiceField(widget=forms.Select(attrs={'class':'form-control'}),
+		choices=platforms)
+	game = forms.ChoiceField(widget=forms.Select(attrs={'class':'form-control'}),
+		choices=games)
+	nickname = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}),max_length=20)
